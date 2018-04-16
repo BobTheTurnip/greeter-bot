afa2a0abb5357ae501c5552609441662ce46ebb9
Liam Gregoire <36730986+BobTheTurnip@users.noreply.github.com>
2018-04-16 22:54:14 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,8 +13,11 @@ client.on('message', message => {
     if (/don(\'t|t) do/g.test((message.content).toLowerCase())) {
         message.channel.send('You cant tell me what to do!');
     }
+    if (/\(\╯\°\(\□|\Д)\°\）\╯\︵ \┻\━\┻/g.test((message.content).toLowerCase())) {
+        message.channel.send('\┬\─\┬\ノ\( \º \_ \º\ノ\) Let\'s keep it civil here');
+    }
     if (/\(\☞ﾟ\ヮﾟ\)\☞/g.test(message.content)) {
-        message.channel.send('\( \☞ﾟ\ヮﾟ\)\☞');
+        message.channel.send('        \☜\(ﾟ\ヮﾟ\☜\)');
     }
 });
 
