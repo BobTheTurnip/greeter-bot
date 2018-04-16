6fcb0a4eaf57e02a3bc1c9ccc2962f5d15227589
Liam Gregoire <36730986+BobTheTurnip@users.noreply.github.com>
2018-04-16 20:32:55 +1000

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-
+// Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-
+// Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
@@ -18,14 +18,15 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-        
+            // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
@@ -36,5 +37,3 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
-
-client.login(process.env.BOT_TOKEN);
