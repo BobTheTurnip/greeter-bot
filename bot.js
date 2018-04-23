8ad7af7a698a0339be666920b57e627afcc0c8ea
Liam Gregoire <36730986+BobTheTurnip@users.noreply.github.com>
2018-04-23 14:38:28 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,19 +13,19 @@ client.on('message', message => {
         message.channel.send('\(\╯\°\□\°\）\╯\︵ \┻\━\┻');
     }
     
-    if (/(^|\s)don(\'t|t)($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)don(\'t|t)($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('You cant tell me what to do!');
     }
-    if (/(^|\s)good bot($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)good bot($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('*purrs softly*');
     }
-    if (/(^|\s)bad bot($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)bad bot($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('BobBot will remember that.');
     }
-    if (/(^|\s)hello there($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)hello there($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('General Kenobi!');
     }
-    if (/(^|\s)say hello($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)say hello($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('Hello!');
     }
     if (/(^|\s)w(a|o|u)t($|\s|\?)/g.test((message.content).toLowerCase())) {
@@ -34,13 +34,13 @@ client.on('message', message => {
     if (/(^|\s)listen/g.test((message.content).toLowerCase())) {
         message.channel.send('What? listerning? Who\'s doing that? Certainly not me!');
     }
-    if (/(^|\s)(murder|kill)($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)(murder|kill)($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('*Calls Police*');
     }
-    if (/(^|\s)\(\╯\°\□\°\）\╯\︵ \┻\━\┻($|\s)/g.test((message.content).toLowerCase())) {
+    if (/(^|\s)\(\╯\°\□\°\）\╯\︵ \┻\━\┻($|\s|\?)/g.test((message.content).toLowerCase())) {
         message.channel.send('\┬\─\┬\ノ\( \º \_ \º\ノ\) Let\'s keep it civil here');
     }
-    if (/(^|\s)\(\☞ﾟ\ヮﾟ\)\☞($|\s)/g.test(message.content)) {
+    if (/(^|\s)\(\☞ﾟ\ヮﾟ\)\☞($|\s|\?)/g.test(message.content)) {
         message.channel.send('\\\\\\\\\\\\\☜\(ﾟ\ヮﾟ\☜\)');
     }
 });
