e3bde00d02f7c48e6a1819f37356d34a780ed12f
Liam Gregoire <36730986+BobTheTurnip@users.noreply.github.com>
2018-05-27 23:39:27 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ client.on('message', message => {
         message.channel.send('\\\\\\\\\\\\\☜\(ﾟ\ヮﾟ\☜\)');
     }
     
-    var temp = new Date(1111, 11, 27, 1, 1, 1, 1);
+    var temp = new Date.now();
     if(temp.getDay() == newGreeting)
     {
         newGreeting += 1;
